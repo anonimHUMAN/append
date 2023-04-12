5dc50ebe52e67433cf182e91eebbf34e0267e079
unknown <otabekovogabek07@gmail.com>
2023-04-12 17:31:07 +0500

    Something

--- src/ALL.jsx
+++ src/ALL.jsx
@@ -3,22 +3,23 @@ import { useRef } from "react"
 import data from "./data"
 import "./section.css"
 export default () => {
-    console.log();
+    const [data1, setData] = useState([])
     const inp2 = useRef(null)
+
     function getNamePhone() {
         console.log(inp2.current.value);
-        // data.push({
-        //     name: "A"
-        // })
+        data1.push(inp2.current.value)
     }
+    // console.log(data);
     document.addEventListener('keydown', (event) => {
         if (event.key == 'Enter') {
             getNamePhone()
+            // console.log("Hello");
         }
     })
     return (
         <div className="container">
-            <div className="left">
+            {/* <div className="left">
                 <a href="/">Cetegory</a><br /><br />
                 <input type="search" placeholder="Add Category" />
                 <div className="all">
@@ -37,18 +38,29 @@ export default () => {
                     <div className="fourth-c"></div>
                     <a href="/payments">Payments</a>
                 </div>
-            </div>
+            </div> */}
+
+
             <div className="right">
                 <h1>All Tasks</h1>
-                <input ref={inp2} type="search" placeholder="Add a new task insdie ‘All’ category" /><br />
+                <div className="div">
+                    <input ref={inp2} type="search" placeholder="Add a new task insdie ‘All’ category" /><br />
+                    <button onClick={getNamePhone}>Click</button>
+                </div>
                 {data.map(item => {
+                    console.log(data);
+                    console.log(item.name);
                     <div className="all">
                         <input id="inp2" type="checkbox" className="inp2" />
-                        <h2>{item.change}</h2>
+                        <h2>{item.name}</h2>
                         <span><p>Uncategorized </p><i><ion-icon name="trash-outline"></ion-icon></i></span>
                     </div>
                 })}
-                <div className="all">
+
+
+
+
+                {/* <div className="all">
                     <input type="checkbox" className="inp2" />
                     <h2>Purchase Milk & Corn Flakes</h2>
                     <span><p>Uncategorized </p><i><ion-icon name="trash-outline"></ion-icon></i></span>
@@ -67,7 +79,7 @@ export default () => {
                     <input type="checkbox" className="inp2" />
                     <h2>Replace laptop's screen</h2>
                     <span><p>Uncategorized </p><i><ion-icon name="trash-outline"></ion-icon></i></span>
-                </div>
+                </div> */}
             </div>
         </div>
     )
